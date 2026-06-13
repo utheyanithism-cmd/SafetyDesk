69abd297eafb423dca01fff92683cb75c45a460c
TarunKumar369 <TarunKumar.Tadala@cognizant.com>
2026-06-13 23:42:37 +0530

    added entity and dtos

--- src/main/java/com/cts/controller/UserController.java
+++ src/main/java/com/cts/controller/UserController.java
@@ -17,6 +17,10 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    
+    public UserController(UserService userService) {
+    	this.userService = userService;
+    }
 
     // POST TYPE URL = /api/users/register 
     @PostMapping("/register")
